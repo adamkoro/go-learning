68a2a8927fd210310aa894ed0b14c420b4bc62f9
Adamkoro <akoronics@protonmail.com>
2022-07-18 19:13:49 +0200

    Concurrency example

---
+++ concurrency/main.go
@@ -0,0 +1,44 @@
+//Code from https://medium.com/wesionary-team/practical-example-of-concurrency-on-golang-fc4609ea8ed1
+// Thanks guys :D
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	websites := []string{
+		"https://stackoverflow.com/",
+		"https://github.com/",
+		"https://www.linkedin.com/",
+		"http://medium.com/",
+		"https://golang.org/",
+		"https://www.udemy.com/",
+		"https://www.coursera.org/",
+		"https://wesionary.team/",
+	}
+
+	for _, website := range websites {
+		go getWebsite(website)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+
+}
+func getWebsite(website string) {
+	defer wg.Done()
+	if res, err := http.Get(website); err != nil {
+		fmt.Println(website, "is down")
+
+	} else {
+		fmt.Printf("[%d] %s is up\n", res.StatusCode, website)
+	}
+
+}
